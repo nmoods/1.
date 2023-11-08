1e75a682d3f8ba8cd2ed777f4ef1a36a8651a83b
Neehal Moodhoo <neehal.codevigor@gmail.com>
2023-11-08 22:55:55 +0400

    Spaces between objects in Js file

--- myScript.js
+++ myScript.js
@@ -9,24 +9,24 @@ let flights = [
         remarks:"ON TIME"
     },
     {
-             time: "03.03",
-             destination: "ALBION",
-             flight: "AO 344",
-             gate:"A 90",
-             remarks:"CANCELLED"
+        time: "03.03",
+        destination: "ALBION",
+        flight: "AO 344",
+        gate:"A 90",
+        remarks:"CANCELLED"
     },
     {
-                  time: "04.58",
-                  destination: "Mauritius",
-                  flight: "BO 720",
-                  gate:"B 04",
-                  remarks:"ON TIME"
+        time: "04.58",
+        destination: "Mauritius",
+        flight: "BO 720",
+        gate:"B 04",
+        remarks:"ON TIME"
     }
     {
-                       time: "08.24",
-                       destination: "LONDON",
-                       flight: "AO 904",
-                       gate:"A 01",
-                       remarks:"ON TIME"
+        time: "08.24",
+        destination: "LONDON",
+        flight: "AO 904",
+        gate:"A 01",
+        remarks:"ON TIME"
     }
 ]
